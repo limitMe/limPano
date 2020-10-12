2184f41666ceda0093a23964b8db3dd8bbfb528d
典少 <zhongdian.zd@alibaba-inc.com>
2020-10-12 16:04:57 +0800

    Initial Commit

---
+++ limPano/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  limPano
+//
+//  Created by zhongdian on 2020/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ limPano/limPanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  limPanoApp.swift
+//  limPano
+//
+//  Created by zhongdian on 2020/10/12.
+//
+
+import SwiftUI
+
+@main
+struct limPanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
