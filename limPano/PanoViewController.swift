dafd951b349b467884ac425dc4d6ccb48541f4ee
典少 <zhongdian.zd@alibaba-inc.com>
2020-10-13 18:11:25 +0800

    feat: Basic structure. MTKView and wrapper it for SwiftUI using UIViewControllerRepresentable

---
+++ limPano/PanoViewController.swift
@@ -0,0 +1,53 @@
+//
+//  PanoViewController.swift
+//  limPano
+//
+//  Created by zhongdian on 2020/10/13.
+//
+
+import UIKit
+import SwiftUI
+import MetalKit
+
+struct PanoView: UIViewControllerRepresentable {
+    typealias UIViewControllerType = PanoViewController
+    
+    func makeUIViewController(context: Context) -> PanoViewController {
+        let panoVC = PanoViewController()
+        return panoVC
+    }
+    
+    func updateUIViewController(_ uiViewController: PanoViewController, context: Context) {
+        // left blanc
+    }
+}
+
+class PanoViewController: UIViewController {
+    private var _mtkView: MTKView!
+    private var _renderer: Renderer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        _mtkView = MTKView(frame: self.view.frame)
+        self.view = _mtkView
+
+        _renderer = Renderer()
+        _mtkView.delegate = _renderer
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touch began")
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touch moved")
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touch ended")
+    }
+    
+    override func touchesEstimatedPropertiesUpdated(_ touches: Set<UITouch>) {
+        print("estimated properties updated")
+    }
+}
---
+++ limPano/Render.swift
@@ -0,0 +1,19 @@
+//
+//  Render.swift
+//  limPano
+//
+//  Created by zhongdian on 2020/10/13.
+//
+
+import UIKit
+import MetalKit
+
+class  Renderer: NSObject, MTKViewDelegate {
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        
+    }
+    
+    func draw(in view: MTKView) {
+        
+    }
+}
